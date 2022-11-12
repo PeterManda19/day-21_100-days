6cf6710ca220bb7cf7842dd8a4503e97afbde327
Peter Manda <109303643+PeterManda19@users.noreply.github.com>
2022-11-12 14:45:38 +0000

    Math Game fully functional

--- main.py
+++ main.py
@@ -7,14 +7,32 @@ print()
 print("Everytime you key in your response press enter. Best score 10. Goodluck!!!")
 
 def showWinMessage(count):
-  print("You got",count,"out of 10.")  
-  print() 
+  if count > 4 and count <= 6:
+    print()
+    print("Good effort!")
+    print("You got",count,"out of 10.")  
+    print() 
+  elif count <=4:
+    print()
+    print("Keep practicing you will get better!")
+    print("You got",count,"out of 10.")  
+    print() 
+  elif count >=7 and count <=9:
+    print()
+    print("Awesome!")
+    print("You got",count,"out of 10.")  
+    print() 
+  elif count == 10:
+    print()
+    print("Phenomenal! 😁😁😁")
+    print("You got",count,"out of 10.")  
+    print() 
+    
   
 
-
-ansList = []
-
-while True:
+quitList = ["y","yes", 'quit', 'exit']
+exit = ""
+while exit.lower() not in quitList:
   print()
   try:
     num = int(input("Name your multiples by entering a number of your choice. "))
@@ -25,37 +43,34 @@ while True:
     print()
     continue
 
-  if num < 0:
-    print("I don't like zero and negative numbers. Goodbye.")
-    break 
+  if num <= 0:
+    print("I don't like zero and negative numbers.")
+    continue 
   else:
-    # ansList.clear()
+    count = 0
     for i in range(1,11):
-      # print(i,"X",num,"=",i*num)
-      ansList.append(i*num)
       print(i,"X",num,"= ")
-      count = 0
-      while count < 11:
-        count+=1
+      try:
+        answer = int(input())
         print()
-        try:
-          answer = int(input())
-          print()
-        except ValueError:
-          print()
-          print("I am expecting positive numbers.")
-          print()
-          continue
-        print(i,"X",num,"= ")  
-        if answer in ansList:
-          print("Well done!")
-        else:
-          print("Nope. The answer was",i*num)
-    print(ansList)    
+      except ValueError:
+        print()
+        print("Nope. The answer was",i*num)
+        print()
+        print("I am expecting positive numbers.")
+        print()
+        continue 
+      if answer == i*num:
+        count+=1
+        print("Correct!")
+        print()
+      else:
+        print("Nope. The answer was",i*num)
+    showWinMessage(count) 
+
+    exit = input("Would like to exit the game? ")
      
-      
-
-
+  
 def endGame():
   while True:
     print()
